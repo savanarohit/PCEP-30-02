a3a5462e4f4349bca363a1dd3856ac4ecc67ed11
Savana Rohit <rohitsavana18@gmail.com>
2023-05-08 07:44:34 +0530

    first commit

---
+++ 1_introduction/introduction.py
@@ -0,0 +1,15 @@
+# This is a basic python code
+print("Hello World!")
+print("Hello", "World!")
+print(
+    """Hello World this
+      is multiline text"""
+)
+
+# Test - Let's practice the print() function. Show the following message to the output:
+# I'm learning to become a Python developer!
+# I'm so excited!
+
+# Remember that the second sentence must appear on a separate line. Remember that any typo in your solution will make it marked as incorrect.
+
+print("I'm learning to become a Python developer!\nI'm so excited!")
