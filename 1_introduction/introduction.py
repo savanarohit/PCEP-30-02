f75fb61c3ff8e07fff7ee705f1271b37812a4786
Savana Rohit <rohitsavana18@gmail.com>
2023-05-08 07:50:36 +0530

    Update introduction.py file

--- 1_introduction/introduction.py
+++ 1_introduction/introduction.py
@@ -10,6 +10,7 @@ print(
 # I'm learning to become a Python developer!
 # I'm so excited!
 
-# Remember that the second sentence must appear on a separate line. Remember that any typo in your solution will make it marked as incorrect.
+# Remember that the second sentence must appear on a separate line.
+# Remember that any typo in your solution will make it marked as incorrect.
 
 print("I'm learning to become a Python developer!\nI'm so excited!")
